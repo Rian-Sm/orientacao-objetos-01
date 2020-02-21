fce57af750a8e8f8cc44492cc7a26355e94aa001
rian santos <riansm100@gmail.com>
2020-02-21 00:30:24 -0300

    teste de prensenca de atributos adicionado

--- src/test/java/exercicios/FuncionarioTeste.java
+++ src/test/java/exercicios/FuncionarioTeste.java
@@ -11,4 +11,18 @@ public class FuncionarioTeste {
         Funcionario p1 = new Funcionario();
         assertNotNull(p1);
     }
+
+    @Test
+    public void presencaAtributos(){
+        Funcionario p1 = new Funcionario();
+        p1.nome = "João da Silva";
+        p1.email = "joao@email.com";
+        p1.idade = 22;
+        p1.salario = 3000.0;
+
+        assertNotNull(p1.nome);
+        assertNotNull(p1.email);
+        assertNotNull(p1.idade);
+        assertNotNull(p1.salario);
+    }
 }
