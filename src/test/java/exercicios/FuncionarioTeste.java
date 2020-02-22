9bb5056474c5a1792bd524b1a64865474f5cf7ac
rian santos <riansm100@gmail.com>
2020-02-21 22:16:36 -0300

    adicionado testes de metodo

--- src/test/java/exercicios/FuncionarioTeste.java
+++ src/test/java/exercicios/FuncionarioTeste.java
@@ -2,6 +2,7 @@ package exercicios;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class FuncionarioTeste {
@@ -24,5 +25,28 @@ public class FuncionarioTeste {
         assertNotNull(p1.email);
         assertNotNull(p1.idade);
         assertNotNull(p1.salario);
+    }
+
+    @Test
+    public void metodoObterDados(){
+        Funcionario p1 = new Funcionario();
+        p1.nome = "João da Silva";
+        p1.email = "joao@email.com";
+        p1.idade = 22;
+        String valorEsperado = (p1.nome +", " + p1.idade + " anos (" + p1.email + ")" );
+
+        assertEquals(valorEsperado, p1.obterDados());
+    }
+
+    @Test
+    public void metodoPromover(){
+        Funcionario p1 = new Funcionario();
+        Double porcentagemAulmento = 10.0;
+        p1.salario = 3000.0;
+        Double valorEsperado = p1.salario + (p1.salario * porcentagemAulmento)/100;
+
+        p1.promover(porcentagemAulmento);
+
+        assertEquals(valorEsperado, p1.salario);
     }
 }
